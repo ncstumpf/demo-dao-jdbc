ca0c6c94b3fea713a786489ada53bda0f7ab1e11
Nicolas Stumpf <ncs.stumpf@gmail.com>
2020-07-08 20:41:50 -0300

    findById

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
+import model.entities.Department;
 
+public interface DepartmentDao {
+	
+	void insert (Department dep);
+	void update (Department dep);
+	void deleteById(Integer Id);
+	Department findById (Integer Id);
+	 List<Department> findAll();
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,12 @@
 package model.dao;
+import java.util.List;
+import model.entities.Seller;
 
-public class SellerDao {
+public interface SellerDao {
 
+	void insert(Seller seller);
+	void update(Seller seller);
+	void deleteById(Integer Id);
+	Seller findById(Integer Id);
+	List<Seller> findAll();
 }
